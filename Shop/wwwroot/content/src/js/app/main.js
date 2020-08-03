af603d5e3099b3c2adf0dc0e0597408ae8f93605
Kinga <kinga123kon@gmail.com>
2020-08-03 20:48:58 +0200

    Shop commit

---
+++ Shop/wwwroot/content/src/js/app/main.js
@@ -0,0 +1,31 @@
+﻿var app = angular.module("myApp", ["ngRoute"]);
+app.config(function ($routeProvider, $locationProvider) {
+    $(document).ready(function () {
+        $('.toast').toast('show');
+    });
+    $routeProvider
+        .when("/", {
+            templateUrl: "main.html"
+        })
+        .when("/wysylka", {
+            templateUrl: "wysylka.html"
+        })
+        .when("/onas", {
+            templateUrl: "onas.html"
+        })
+        .when("/produkty", {
+            templateUrl: "produkty.html"
+        })
+        .when("/koszyk", {
+            templateUrl: "koszyk.html"
+        })
+        .when("/kontakt", {
+            templateUrl: "kontakt.html",
+        });
+
+    $locationProvider.html5Mode(true);
+
+});
+
+
+
